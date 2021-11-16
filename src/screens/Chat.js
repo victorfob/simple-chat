d136bc6bc8f8ed0f5f1cd9d652dc8181ef932f83
Victor Brayner <victor.brayner@hotmail.com>
2021-11-16 20:39:16 -0300

    update: tests

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,4 +1,9 @@
-import React, { useCallback, useLayoutEffect, useState } from "react";
+import React, {
+  useCallback,
+  useLayoutEffect,
+  useEffect,
+  useState,
+} from "react";
 import { View, TouchableOpacity } from "react-native";
 import { Avatar } from "react-native-elements";
 import { GiftedChat } from "react-native-gifted-chat";
@@ -10,6 +15,7 @@ import { auth, db } from "../services/firebase";
 
 const Chat = ({ navigation, route }) => {
   const [messages, setMessages] = useState([]);
+  const [timer, setTimer] = useState(0);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -36,29 +42,63 @@ const Chat = ({ navigation, route }) => {
       GiftedChat.append(previousMessages, messages)
     );
 
-    const { _id, createdAt, text, user } = messages[0];
+    console.log(messages);
 
-    // Salva mensagem no chat do remetente
-    db.collection("chats")
-      .doc(auth?.currentUser?.email)
-      .collection(route.params.user2)
-      .add({
-        _id,
-        createdAt,
-        text,
-        user,
-      });
-    // Salva mensagem no chat do destinatário
-    db.collection("chats")
-      .doc(route.params.user2)
-      .collection(auth?.currentUser?.email)
-      .add({
-        _id,
-        createdAt,
-        text,
-        user,
-      });
+    // const { _id, createdAt, text, user } = messages[0];
+
+    let interval = setInterval(() => {
+      setTimer((previous) => previous + 0.1);
+    }, 100);
+
+    const array = [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+      40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+      58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
+      76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93,
+      94, 95, 96, 97, 98, 99, 100,
+    ];
+
+    const results = array.map(async (value) => {
+      let _id = "22345" + value,
+        createdAt = new Date(),
+        text = "teste 2 - " + value,
+        user = {
+          _id: "victor.brayner@hotmail.com",
+          avatar: "https://www.trackergps.com/canvas/images/icons/avatar.jpg",
+          name: "Victor Brayner",
+        };
+
+      // Salva mensagem no chat do remetente
+      await db
+        .collection("chats")
+        .doc(auth?.currentUser?.email)
+        .collection(route.params.user2)
+        .add({
+          _id,
+          createdAt,
+          text,
+          user,
+        });
+      // Salva mensagem no chat do destinatário
+      await db
+        .collection("chats")
+        .doc(route.params.user2)
+        .collection(auth?.currentUser?.email)
+        .add({
+          _id,
+          createdAt,
+          text,
+          user,
+        });
+    });
+    await Promise.all(results);
+    clearInterval(interval);
   }, []);
+
+  useEffect(() => {
+    console.log(timer);
+  }, [timer]);
 
   useLayoutEffect(() => {
     return (
